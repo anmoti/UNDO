cd939deae47991f260ea1a99cf4de56aa31bf362
anmoti <77442643+anmoti@users.noreply.github.com>
2025-10-01 11:34:06 +0000

    turbidity_to_bods を公開にする

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :passwords, param: :token
+  resources :turbidity_to_bods
 
   if Rails.env.development? || Rails.env.test?
-      resources :turbidity_to_bods
       resources :reviews
       resources :users
   end
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
