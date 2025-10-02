2b7dcc220672d978706e31f48ae5e175d06ee4a7
anmoti <77442643+anmoti@users.noreply.github.com>
2025-10-02 06:22:00 +0000

    fix: :bug: 正常にイベントリスナーが消えるように

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
+/** @import { Context } from "@hotwired/stimulus"  */
+
 const EVENTS = {
     SHOW: "comment:show",
 };
@@ -7,17 +9,19 @@ const EVENTS = {
 // Connects to data-controller="comments"
 /** @extends {Controller<HTMLDivElement>} */
 export default class extends Controller {
+    boundShow = this.show.bind(this);
+
     connect() {
         console.log("Comments controller connected");
 
         // 初期状態では非表示
         this.hide();
 
-        window.addEventListener(EVENTS.SHOW, this.show.bind(this));
+        window.addEventListener(EVENTS.SHOW, this.boundShow);
     }
 
     disconnect() {
-        window.removeEventListener(EVENTS.SHOW, this.show.bind(this));
+        window.removeEventListener(EVENTS.SHOW, this.boundShow);
     }
 
     show() {
