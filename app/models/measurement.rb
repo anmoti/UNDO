71b07bdd9c3fe315189b6ab202366c04866274f9
anmoti <77442643+anmoti@users.noreply.github.com>
2025-10-06 08:38:13 +0000

    Rails8 の新しいEnum構文に対応

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,5 +1,5 @@
 class Measurement < ApplicationRecord
   belongs_to :submitter, class_name: "User"
 
-  enum status: { pending: 0, predicted: 1, validated: 2, anomaly: 3 }
+  enum :status, { pending: 0, predicted: 1, validated: 2, anomaly: 3 }
 end
