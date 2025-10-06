d660772a7c31ea17fe5f471c3868e78ffc654293
anmoti <77442643+anmoti@users.noreply.github.com>
2025-10-06 15:00:11 +0000

    kyをダウンロード

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,6 +11,8 @@ else
   pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/+esm"
   pin "@googlemaps/js-api-loader", to: "https://cdn.jsdelivr.net/npm/@googlemaps/js-api-loader@1.16.10/+esm"
 end
+# ローカルからロードできないのでCDNからロード
+pin "ky", to: "https://cdn.jsdelivr.net/npm/ky@1.11.0/+esm"
 
 pin "application"
 pin_all_from "app/javascript/controllers", under: "controllers"
