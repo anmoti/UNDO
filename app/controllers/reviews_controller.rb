4ea767b5434811f3d436c314e224a9bc44ea4e02
SANADA Euki <yuu.mat.930@gmail.com>
2025-10-10 01:03:18 +0900

    feat: reviewの不要な機能を消去

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,6 @@
 class ReviewsController < ApplicationController
   layout "main", only: [ :new ]
-  allow_unauthenticated_access only: %i[index show]
-  before_action :set_review, only: %i[ show edit update destroy ]
   before_action :check_company_account, only: %i[ new create ]
-
-  # GET /reviews or /reviews.json
-  def index
-    @reviews = Review.all
-
-    # reviewee_idでフィルタリング
-    if params[:reviewee_id].present?
-      @reviews = @reviews.where(reviewee_id: params[:reviewee_id])
-    end
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @reviews }
-    end
-  end
-
-  # GET /reviews/1 or /reviews/1.json
-  def show
-  end
 
   # GET /reviews/new
   def new
@@ -33,10 +12,6 @@ class ReviewsController < ApplicationController
       @review.reviewee_id = params[:reviewee_id]
       @store = Store.find_by(id: params[:reviewee_id])
     end
-  end
-
-  # GET /reviews/1/edit
-  def edit
   end
 
   # POST /reviews or /reviews.json
@@ -55,35 +30,7 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1 or /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: t("flash.reviews.updated") }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1 or /reviews/1.json
-  def destroy
-    @review.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to root_path, status: :see_other, notice: t("flash.reviews.destroyed") }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
-
   # 企業アカウントがレビューを投稿できないようにチェック
   def check_company_account
     if Current.user&.is_company?
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -7,11 +7,6 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
     sign_in_as(@user, password: "passwordcarol")
   end
 
-  test "should get index" do
-    get reviews_url
-    assert_response :success
-  end
-
   test "should get new" do
     get new_review_url
     assert_response :success
@@ -26,29 +21,6 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to root_path
-  end
-
-  test "should show review" do
-    get review_url(@review)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_review_url(@review)
-    assert_response :success
-  end
-
-  test "should update review" do
-    patch review_url(@review), params: { review: { comment: @review.comment, rating: @review.rating, reviewee_id: @review.reviewee_id, reviewer_id: @review.reviewer_id } }
-    assert_redirected_to review_url(@review)
-  end
-
-  test "should destroy review" do
-    assert_difference("Review.count", -1) do
-      delete review_url(@review)
-    end
-
-    assert_redirected_to reviews_url
   end
 
   test "企業アカウントはレビューを投稿できない" do
