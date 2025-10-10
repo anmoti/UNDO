be503be7f2c04ac45acc747ed9566c5840b9e7b7
SANADA Euki <yuu.mat.930@gmail.com>
2025-10-10 20:14:40 +0900

    fix: takeDownTimeのタイムゾーンを固定の「Tokyo」に変更
    ユーザー側からTZ取得はできないため

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,7 +26,7 @@ class HomeController < ApplicationController
         shareInfo: active_share ? {
           itemName: active_share.item_name,
           description: active_share.description,
-          takeDownTime: active_share.take_down_time.in_time_zone(current_user.time_zone).strftime("%Y-%m-%d %H:%M"),
+          takeDownTime: active_share.take_down_time.in_time_zone("Tokyo").strftime("%Y-%m-%d %H:%M"),
           photoUrl: active_share.photo_url
         } : nil
       }
