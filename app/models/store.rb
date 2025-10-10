3230ab2af4225bbcf28c592836e1abca67010714
SANADA Euki <yuu.mat.930@gmail.com>
2025-10-10 12:58:46 +0900

    feat: 企業アカウント-店舗運営者関係モデルの作成

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,4 +5,16 @@ class Store < ApplicationRecord
   validates :lon, numericality: true, allow_nil: true
 
   has_many :received_reviews, class_name: "Review", foreign_key: "reviewee_id", dependent: :destroy
+
+  # 運営者
+  has_many :store_operators, dependent: :destroy
+  has_many :operators, through: :store_operators, source: :user
+
+  # うどんシェア
+  has_many :udon_shares, dependent: :destroy
+
+  # 現在アクティブなシェアを取得
+  def active_udon_share
+    udon_shares.active.order(created_at: :desc).first
+  end
 end
