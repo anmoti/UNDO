d74677233ea3631592f18d64d4bee013012840f1
SANADA Euki <yuu.mat.930@gmail.com>
2025-10-10 19:18:22 +0900

    fix: テストミスを修正

--- app/models/udon_share.rb
+++ app/models/udon_share.rb
@@ -3,6 +3,7 @@ class UdonShare < ApplicationRecord
 
   validates :item_name, presence: true
   validates :description, presence: true
+  validates :take_down_time, presence: true
 
   # 有効期限が切れていないシェアのみを取得
   scope :active, -> { where("take_down_time > ?", Time.current) }
